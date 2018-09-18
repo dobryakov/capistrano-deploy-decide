39e79770b3ba99a75c22925336cb5bca459179d1
Grigoriy Dobryakov <grigoriydobryakov@gmail.com>
2018-09-18 15:00:18 +0300

    it works

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "cap-test"
+set :repo_url, "/home/dg/projects/cap-test/dev"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/dg/projects/cap-test/prod/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -33,7 +34,46 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :deploy_via, :copy
+set :copy_exclude, [".svn", ".git"]
+
+after 'deploy:publishing', 'deploy:decide'
+
+namespace :deploy do
+
+  task :decide do
+    on roles(:app) do
+      #e = capture('date')
+      #execute("echo " + e)
+      p = shared_path.to_s + "/releases-count.txt"
+      execute("touch " + p)
+      release_number = ( capture("cat " + p) ).to_i + 1
+      #execute("echo " + release_number.to_s)
+      execute("echo " + release_number.to_s + " > " + p)
+      if (release_number % fetch(:keep_releases)).zero?
+        invoke 'deploy:restart'
+      else
+        invoke 'deploy:reload'
+      end
+    end
+  end
+
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "echo restart"
+    end
+  end
+
+  desc 'Reload application'
+  task :reload do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "echo reload"
+    end
+  end
+
+end
